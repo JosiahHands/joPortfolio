5f021a8a908db137f3ef104bb978cd6749475661
Josiah Hands <josiahwhands@gmail.com>
2026-09-22 07:30:50 -0500

    HTML: defer => async

--- game/script/game.js
+++ game/script/game.js
@@ -5,13 +5,13 @@ const ranN = document.querySelector('#ranN');
 const botS = document.querySelector('#botS');
 const plyS = document.querySelector('#plyS');
 // table 2
-const plyG = document.querySelector('#plyG');
+const urG = document.querySelector('#plyG');
 const botG = document.querySelector('#botG');
 const wnr = document.querySelector('#wnr');
 // table 3
 const rnd = document.querySelector('#rnd');
 // player input
-const urG = document.querySelector('#urG');
+const plyG = document.querySelector('#plyG');
 const sbmt = document.querySelector('#sbmt');
 const nxt = document.querySelector('#nxt');
 // program
@@ -22,29 +22,34 @@ const submit = () => {
     sbmt.hidden = true;
     nxt.hidden = false;
     ranN.innerHTML = rand.toString();
-    plyG.innerHTML = urG.innerHTML.toString();
+    urG.innerHTML = plyG.innerHTML;
     botG.innerHTML = randBot.toString();
     const winner = () => {
-        const urAb = Math.abs(urG.innerHTML - rand);
+        const urAb = Math.abs(plyG - rand);
         const botAbs = Math.abs(randBot - rand);
         if (urAb < botAbs) {
             return true;
         }
-        else if (urAb == botAbs) {
+        else if (urAb === botAbs) {
             return true;
         }
         ;
         return false;
     };
-    if (winner() == true) {
+    if (winner() === true) {
         wnr.innerHTML = 'You won!';
         plyS.innerHTML += 1;
     }
-    else {
+    else if (winner() === false) {
         wnr.innerHTML = 'Computer won!';
         botS.innerHTML += 1;
     }
+    else {
+        wnr.innerHTML = 'Error!';
+    }
 };
 sbmt.onclick = submit;
+const next = () => {
+};
 export {};
 //# sourceMappingURL=game.js.map
--- game/script/game.ts
+++ game/script/game.ts
@@ -5,13 +5,13 @@
         const botS = document.querySelector('#botS') as HTMLElement;
         const plyS = document.querySelector('#plyS') as HTMLElement;
     // table 2
-        const plyG = document.querySelector('#plyG') as HTMLElement;
+        const urG = document.querySelector('#plyG') as HTMLElement;
         const botG = document.querySelector('#botG') as HTMLElement;
         const wnr = document.querySelector('#wnr') as HTMLElement;
     // table 3
         const rnd = document.querySelector('#rnd') as HTMLElement;
     // player input
-        const urG = document.querySelector('#urG')as any;
+        const plyG = document.querySelector('#plyG')as any;
         const sbmt = document.querySelector('#sbmt') as HTMLElement;
         const nxt = document.querySelector('#nxt') as HTMLElement;
 // program
@@ -24,27 +24,32 @@ const submit = () => {
     sbmt.hidden = true;
     nxt.hidden = false;
     ranN.innerHTML = rand.toString();
-    plyG.innerHTML = urG.innerHTML.toString()
+    urG.innerHTML = plyG.innerHTML;
     botG.innerHTML = randBot.toString();
     const winner = () => {
-        const urAb = Math.abs(urG.innerHTML-rand)
+        const urAb = Math.abs(plyG-rand)
         const botAbs = Math.abs(randBot-rand)
         if (urAb < botAbs) {
             return true;
-        } else if (urAb == botAbs) {
+        } else if (urAb === botAbs) {
             return true;
         };
         return false;
     }
-    if (winner() == true) {
-        wnr.innerHTML = 'You won!'
+    if (winner() === true) {
+        wnr.innerHTML = 'You won!';
         plyS.innerHTML += 1;
-    } else {
-        wnr.innerHTML = 'Computer won!'
+    } else if (winner() === false) {
+        wnr.innerHTML = 'Computer won!';
         botS.innerHTML += 1;
+    } else {
+        wnr.innerHTML = 'Error!';
     }
 }
 
 sbmt.onclick = submit;
 
+const next = () => {
+    
+}
 
