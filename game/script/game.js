d34f371a06c83c9918eeebdb10b97426bca3b8bc
Josiah Hands <josiahwhands@gmail.com>
2026-09-25 11:23:38 -0500

    added game

--- game/script/game.js
+++ game/script/game.js
@@ -5,27 +5,56 @@ const ranN = document.querySelector('#ranN');
 const botS = document.querySelector('#botS');
 const plyS = document.querySelector('#plyS');
 // table 2
-const urG = document.querySelector('#plyG');
+const urG = document.querySelector('#urG');
 const botG = document.querySelector('#botG');
 const wnr = document.querySelector('#wnr');
 // table 3
 const rnd = document.querySelector('#rnd');
-// player input
+// input
 const plyG = document.querySelector('#plyG');
 const sbmt = document.querySelector('#sbmt');
 const nxt = document.querySelector('#nxt');
+const botInput = document.querySelector('#botInput');
+const reset = document.querySelector('#reset');
+const add = document.querySelector('#add');
+const sub = document.querySelector('#sub');
 // program
-const rand = (Math.floor(Math.random() * 9) + 1);
-const randBot = (Math.floor(Math.random() * 9) + 1);
-nxt.hidden = true;
+const rand1 = () => {
+    const num = Math.floor(Math.random() * 9) + 1;
+    return num;
+};
+const rand2 = () => {
+    const num = Math.floor(Math.random() * 9) + 1;
+    return num;
+};
+const addScore = (el) => {
+    let num = Number(el.innerHTML);
+    num += 1;
+    el.innerHTML = num.toString();
+};
+sbmt.style.display = 'block';
+nxt.style.display = 'none';
+const block = () => {
+    if (sbmt.style.display === 'block') {
+        sbmt.style.display = 'none';
+        nxt.style.display = 'block';
+    }
+    else {
+        sbmt.style.display = 'block';
+        nxt.style.display = 'none';
+    }
+};
 const submit = () => {
-    sbmt.hidden = true;
-    nxt.hidden = false;
+    block();
+    const rand = rand1();
+    const randBot = rand2();
+    botInput.innerHTML = randBot.toString();
     ranN.innerHTML = rand.toString();
-    urG.innerHTML = plyG.innerHTML;
     botG.innerHTML = randBot.toString();
+    const guess = Number(plyG.value);
+    urG.innerHTML = plyG.value;
     const winner = () => {
-        const urAb = Math.abs(plyG - rand);
+        const urAb = Math.abs(guess - rand);
         const botAbs = Math.abs(randBot - rand);
         if (urAb < botAbs) {
             return true;
@@ -38,11 +67,11 @@ const submit = () => {
     };
     if (winner() === true) {
         wnr.innerHTML = 'You won!';
-        plyS.innerHTML += 1;
+        addScore(plyS);
     }
     else if (winner() === false) {
         wnr.innerHTML = 'Computer won!';
-        botS.innerHTML += 1;
+        addScore(botS);
     }
     else {
         wnr.innerHTML = 'Error!';
@@ -50,6 +79,43 @@ const submit = () => {
 };
 sbmt.onclick = submit;
 const next = () => {
+    block();
+    ranN.innerHTML = '?';
+    botG.innerHTML = '?';
+    wnr.innerHTML = '?';
+    botInput.innerHTML = '?';
+    urG.innerHTML = '?';
+    addScore(rnd);
 };
+nxt.onclick = next;
+const resetBtn = () => {
+    ranN.innerHTML = '?';
+    botS.innerHTML = '0';
+    plyS.innerHTML = '0';
+    urG.innerHTML = '?';
+    botG.innerHTML = '?';
+    wnr.innerHTML = '?';
+    rnd.innerHTML = '?';
+    botInput.innerHTML = '?';
+};
+reset.onclick = resetBtn;
+const addInput = (el) => {
+    let num = Number(el.value);
+    num += 1;
+    el.value = num.toString();
+};
+const subInput = (el) => {
+    let num = Number(el.value);
+    num -= 1;
+    el.value = num.toString();
+};
+const addVal = () => {
+    addInput(plyG);
+};
+const subVal = () => {
+    subInput(plyG);
+};
+add.onclick = addVal;
+sub.onclick = subVal;
 export {};
 //# sourceMappingURL=game.js.map
--- game/script/game.ts
+++ game/script/game.ts
@@ -5,29 +5,56 @@
         const botS = document.querySelector('#botS') as HTMLElement;
         const plyS = document.querySelector('#plyS') as HTMLElement;
     // table 2
-        const urG = document.querySelector('#plyG') as HTMLElement;
+        const urG = document.querySelector('#urG') as HTMLElement;
         const botG = document.querySelector('#botG') as HTMLElement;
         const wnr = document.querySelector('#wnr') as HTMLElement;
     // table 3
         const rnd = document.querySelector('#rnd') as HTMLElement;
-    // player input
-        const plyG = document.querySelector('#plyG')as any;
+    // input
+        const plyG = document.querySelector('#plyG')as HTMLInputElement;
         const sbmt = document.querySelector('#sbmt') as HTMLElement;
         const nxt = document.querySelector('#nxt') as HTMLElement;
+        const botInput = document.querySelector('#botInput') as HTMLElement;
+        const reset = document.querySelector('#reset') as HTMLElement;
+        const add = document.querySelector('#add') as HTMLElement;
+        const sub = document.querySelector('#sub') as HTMLElement;
 // program
-const rand: number | string = (Math.floor(Math.random()*9)+1);
-const randBot: number | string = (Math.floor(Math.random()*9)+1);
+const rand1 = (): number => {
+    const num = Math.floor(Math.random()*9)+1;
+    return num;
+};
+const rand2 = (): number => {
+    const num = Math.floor(Math.random()*9)+1;
+    return num;
+};
 
-nxt.hidden = true;
-
+const addScore = (el: HTMLElement): void => {
+    let num = Number(el.innerHTML)
+    num += 1;
+    el.innerHTML = num.toString();
+}
+    sbmt.style.display = 'block';
+    nxt.style.display = 'none';
+const block = () => {
+    if (sbmt.style.display === 'block') {
+        sbmt.style.display = 'none';
+        nxt.style.display = 'block';
+    } else {
+        sbmt.style.display = 'block';
+        nxt.style.display = 'none';
+    }
+} 
 const submit = () => {
-    sbmt.hidden = true;
-    nxt.hidden = false;
+    block();
+    const rand = rand1();
+    const randBot = rand2();
+    botInput.innerHTML = randBot.toString();
     ranN.innerHTML = rand.toString();
-    urG.innerHTML = plyG.innerHTML;
     botG.innerHTML = randBot.toString();
+    const guess = Number(plyG.value);
+    urG.innerHTML = plyG.value;
     const winner = () => {
-        const urAb = Math.abs(plyG-rand)
+        const urAb = Math.abs(guess-rand)
         const botAbs = Math.abs(randBot-rand)
         if (urAb < botAbs) {
             return true;
@@ -38,10 +65,10 @@ const submit = () => {
     }
     if (winner() === true) {
         wnr.innerHTML = 'You won!';
-        plyS.innerHTML += 1;
+        addScore(plyS)
     } else if (winner() === false) {
         wnr.innerHTML = 'Computer won!';
-        botS.innerHTML += 1;
+        addScore(botS)
     } else {
         wnr.innerHTML = 'Error!';
     }
@@ -50,6 +77,47 @@ const submit = () => {
 sbmt.onclick = submit;
 
 const next = () => {
-    
+    block()
+    ranN.innerHTML = '?';
+    botG.innerHTML = '?';
+    wnr.innerHTML = '?';
+    botInput.innerHTML = '?';
+    urG.innerHTML = '?';
+    addScore(rnd);
+
 }
 
+nxt.onclick = next;
+
+const resetBtn = () => {
+     ranN.innerHTML = '?'     
+     botS.innerHTML = '0'
+     plyS.innerHTML = '0'
+     urG .innerHTML = '?'
+     botG.innerHTML = '?'
+     wnr.innerHTML = '?'
+     rnd.innerHTML = '?'
+     botInput.innerHTML = '?'
+}
+reset.onclick = resetBtn;
+
+const addInput = (el: HTMLInputElement): void => {
+    let num = Number(el.value)
+    num += 1;
+    el.value = num.toString();
+}
+const subInput = (el: HTMLInputElement): void => {
+    let num = Number(el.value)
+    num -= 1;
+    el.value = num.toString();
+}
+
+const addVal = () => {
+    addInput(plyG)
+}
+const subVal = () => {
+    subInput(plyG)
+}
+
+add.onclick = addVal;
+sub.onclick = subVal;
